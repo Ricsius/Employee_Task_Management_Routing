2d6fb915045dadf837fc01f89b98883b0784d7ca
Ricsius <hajnal.ricsi@gmail.com>
2025-03-12 22:26:38 +0100

    Add static data to route

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -13,6 +13,7 @@ export class UserTasksComponent implements OnInit {
   userId = input.required<string>();
   userName = '';
   //userName = computed(() => this.usersService.users.find(u => u.id === this.userId())?.name);
+  message = input.required<string>();
   
   constructor(private destroyRef: DestroyRef, private usersService: UsersService, private activatedRoute: ActivatedRoute) {}
 
@@ -21,6 +22,8 @@ export class UserTasksComponent implements OnInit {
     console.log(this.activatedRoute);
     console.log('ActivatedRoute snapshot');
     console.log(this.activatedRoute.snapshot);
+    console.log('Static message');
+    console.log(this.message());
 
     const subscription = this.activatedRoute.paramMap.subscribe((paramMap) => {
       const uid = paramMap.get('userId');
